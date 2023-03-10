0264c462c46d125dc55aa7913058b9413101785f
dream <1016835016@qq.com>
2023-03-10 16:10:53 +0800

    配置完成和创建通信类

--- src/main/java/com/blog/util/interceptor/WebMvcConfigurer.java
+++ src/main/java/com/blog/util/interceptor/WebMvcConfigurer.java
@@ -22,6 +22,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
                 .excludePathPatterns("/api/login/**")
                 .excludePathPatterns("/api/Like/getUserLikeType")
                 .excludePathPatterns("/api/search/**")
+                .excludePathPatterns("/imserver/**")
         ;                    // 拦截所有请求，通过判断是否有 @LoginRequired 注解 决定是否需要登录
         super.addInterceptors(registry);
     }
